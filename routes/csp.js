d00ff31ee654ca9ca5efce90d1705fd5aaee4c9b
ajay-appknox <ajay@appknox.com>
2025-05-16 14:50:57 +0530

    Updated CSP Header

--- routes/csp.js
+++ routes/csp.js
@@ -2,7 +2,7 @@ const app = require('express');
 const router = app.Router();
 
 router.get('/api/csp/insecure/', (req, res) => {
-	const csp = "default-src 'self'; script-src 'https://example.com'";
+	const csp = "script-src 'unsafe-inline' 'unsafe-eval' 'self' data: https://www.google.com http://www.google-analytics.com/gtm/js  https://*.gstatic.com/feedback/ https://ajax.googleapis.com;";
 	res.set("Content-Security-Policy", csp);
 	res.send("CSP HTTP/1.1\n" + csp);
 });
